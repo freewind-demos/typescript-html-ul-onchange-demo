ef0bd3939b41d65104c9951843865331aeec978e
Freewind <nowindlee@gmail.com>
2021-02-24 16:39:28 +0800

    typescript-html-ul-onchange-demo

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-TypeScript HTML Demo
+TypeScript HTML UL OnChange Demo
 =================================
+
+没想到`<ul>`也可以监听onChange事件。传过来的event是内部input等控件的事件。
 
 ```
 npm i
--- src/hello.ts
+++ src/hello.ts
@@ -1,1 +1,6 @@
-console.log('### Hello');
+const ul = document.querySelector<HTMLUListElement>('#main');
+
+ul!.addEventListener('change', (event) => {
+  const input = event.target as HTMLInputElement
+  console.log('### event', input.value);
+})
